badc3b18bc230adff64c781b4786f8c6697cffcd
Nazar <somewhereseen@gmail.com>
2020-04-02 10:33:05 +0300

    Updates

---
+++ precache-manifest.e5469ae1aba76a78e142d6b049ad78e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b03016560f42c572c930950f67afc63",
+    "url": "/startship/index.html"
+  },
+  {
+    "revision": "12366b6ae03779380367",
+    "url": "/startship/static/css/main.e00c9818.chunk.css"
+  },
+  {
+    "revision": "41b9bcf2990da94ff190",
+    "url": "/startship/static/js/2.74523571.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/startship/static/js/2.74523571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12366b6ae03779380367",
+    "url": "/startship/static/js/main.cf4ab197.chunk.js"
+  },
+  {
+    "revision": "55da1d4d42f58ddf8887",
+    "url": "/startship/static/js/runtime-main.458c893d.js"
+  },
+  {
+    "revision": "0ded49c86eb6c80f94b5aab3cce4e8c7",
+    "url": "/startship/static/media/preloader.0ded49c8.gif"
+  },
+  {
+    "revision": "ee632e32d1ee9557e0ab7b337c3fb007",
+    "url": "/startship/static/media/space-seamless-pattern.ee632e32.jpg"
+  },
+  {
+    "revision": "0621d1ea4f01025a7d9f7bf4545d232a",
+    "url": "/startship/static/media/space-ship.0621d1ea.gif"
+  }
+]);
